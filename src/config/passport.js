d10b8c14d1c3a8658da844a2f079006bb0cb7928
wonee09 <wonjang031@gmail.com>
2025-11-14 14:08:45 +0900

    first commit

---
+++ src/config/passport.js
@@ -0,0 +1,39 @@
+import passport from "passport";
+import localStrategy from "../middlewares/passport/localStrategy.js";
+import userRepository from "../repositories/userRepository.js";
+import jwt from "../middlewares/passport/jwtStrategy.js";
+
+passport.use(localStrategy);
+
+passport.use("access-token", jwt.accessTokenStrategy);
+passport.use("refresh-token", jwt.refreshTokenStrategy);
+
+// 세션 저장 시 req.session 에 user.id 값을 할당합니다.
+passport.serializeUser((user, done) => {
+  done(null, user.id); // req.session.passport.user = user.id
+});
+/* 
+{
+  "세션ID": {
+    "passport": {
+      "user":  user.id // done(null, user.id)에서 전달된 값
+    }
+    // 기타 세션 데이터
+  }
+}
+*/
+
+// req.session.passport.user 값이 첫 번째 매개변수인 id에 할당됩니다.
+passport.deserializeUser(async (id, done) => {
+  try {
+    // id를 이용해 사용자 정보를 조회
+
+    const user = await userRepository.findById(id);
+
+    done(null, user); // req.user = user;
+  } catch (error) {
+    done(error);
+  }
+});
+
+export default passport;
---
+++ src/services/reviewService.ts
@@ -0,0 +1,33 @@
+import reviewRepository from "../repositories/reviewRepository.js";
+import { Review } from "@prisma/client";
+
+async function create(review: Omit<Review, "id" | "createdAt" | "updatedAt">) {
+  return reviewRepository.save(review);
+}
+
+async function getById(id: Review["id"]) {
+  return reviewRepository.getById(id);
+}
+
+async function getAll() {
+  return reviewRepository.getAll();
+}
+
+async function update(
+  id: Review["id"],
+  review: Partial<Omit<Review, "id" | "createdAt" | "updatedAt" | "authorId">>,
+) {
+  return reviewRepository.update(id, review);
+}
+
+async function deleteById(id: Review["id"]) {
+  return reviewRepository.deleteById(id);
+}
+
+export default {
+  create,
+  getById,
+  getAll,
+  update,
+  deleteById,
+};
